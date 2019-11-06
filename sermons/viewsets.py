882fac688d2264d17e207a4b5424df0e04bb2195
Miroslav Shubernetskiy <miroslav@miki725.com>
2019-11-06 09:08:05 -0500

    making series expandable

--- sermons/viewsets.py
+++ sermons/viewsets.py
@@ -4,15 +4,40 @@ from url_filter.integrations.drf_coreapi import CoreAPIURLFilterBackend
 
 from .filtersets import SermonFilterSet, SermonSeriesFilterSet
 from .models import Sermon, SermonSeries
-from .serializers import SermonSerializer, SermonSeriesSerializer
+from .serializers import (
+    JustSermonSeriesSerializer,
+    SermonSerializer,
+    SermonSeriesSerializer,
+)
 
 
 class SermonSeriesViewSet(viewsets.ReadOnlyModelViewSet):
     model = SermonSeries
-    queryset = SermonSeries.objects.prefetch_related("sermons").all()
+    queryset = SermonSeries.objects.all()
     serializer_class = SermonSeriesSerializer
     filter_backends = [CoreAPIURLFilterBackend]
     filter_class = SermonSeriesFilterSet
+
+    @property
+    def is_sermons_expanded(self):
+        return "sermons" in self.request.GET.getlist("expand", [])
+
+    def get_serializer_class(self):
+        if self.is_sermons_expanded:
+            return super().get_serializer_class()
+        else:
+            return JustSermonSeriesSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.is_sermons_expanded:
+            return qs.prefetch_related(
+                "sermons",
+                "sermons__speakers",
+                "sermons__soundcloud_assets",
+                "sermons__youtube_assets",
+            ).all()
+        return qs
 
 
 class SermonViewSet(viewsets.ReadOnlyModelViewSet):
